8fa36e2ecad80c19a3694e493d333c22cbb46269
diogotvf7 <diogotvf7@gmail.com>
2024-03-09 17:57:49 +0000

    Added plotly dash gui

--- src/app.jl
+++ src/app.jl
@@ -11,7 +11,7 @@ app.layout = html_div() do
             data = [
                 (x = ["giraffes", "orangutans", "monkeys"], y = [20, 14, 23], type = "bar", name = "SF"),
                 (x = ["giraffes", "orangutans", "monkeys"], y = [12, 18, 29], type = "bar", name = "Montreal"),
-                (x = ["chimps", "orangutans", "monkeys"], y = [27, 2, 13], type = "bar", name = "Montreal"),
+                (x = ["chimps", "orangutans", "monkeys"], y = [27, 2, 13], type = "bar", name = "Lol "),
             ],
             layout = (title = "Dash Data Visualization", barmode="group")
         )
