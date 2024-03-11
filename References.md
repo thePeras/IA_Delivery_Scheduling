d1cfb1f2a56fbf3e4d3643d13db5ea2a1b32fefd
thePeras <jtopereira@gmail.com>
2024-03-11 12:01:57 +0000

    More on genetic algorithm

--- References.md
+++ References.md
@@ -1,2 +1,3 @@
 Hill Climbing - https://www.youtube.com/watch?v=oSdPmxRCWws
 Tabu Search - https://www.iasj.net/iasj/download/c8717a7cde6ba605
+Genetic Algorithms - https://medium.com/@AnasBrital98/genetic-algorithm-explained-76dfbc5de85d
--- src/algorithms/genetic_algorithm.jl
+++ src/algorithms/genetic_algorithm.jl
@@ -5,23 +5,48 @@ end
 function genetic_algorithm(state::State, max_generations::Int64 = 100, population_size::Int64 = 20, mutation_rate::Float64 = 0.01)
 
     # Generate the initial population
+    population = generate_population(population_size)
 
     # Iterate the population_size
     for _ in 1:population_size
-        # Generate a random individual
-        individual = generate_random_individual(state)
-        push!(population.individuals, individual)
+        # Calculate Population fitness?
+
+        # Reproduction
+        new_population = []
+        # TODO: We can use elitism selection:
+        # Copying the best chromosomes from the current population (based on their fitness) to the next generation without 
+        # crossover or mutation.
+        # This helps ensure that the best solutions are not lost during the reproduction process. 
+        for _ in 1:population_size
+            parent1 = select_from_population(population)
+            parent2 = select_from_population(population)
+            child1, child2 = crossover(parent1, parent2)
+
+            # Mutation
+            if rand(Uniform(0, 1)) < mutation_rate
+                mutate(child1)
+            end
+            if rand(Uniform(0, 1)) < mutation_rate
+                mutate(child2)
+            end
+
+            push!(new_population, child1)
+            push!(new_population, child2)
+        end
+
+        # calculate the fitness of the new population
+        
     end
 
 end
 
 # Select from population function
-# TODO: Modify this function
+# TODO: Modify this function: Tournament selection, Roulette wheel selection, Rank-based selection
 function select_from_population(population::Population)
     return population.individuals[rand(1:length(population.individuals))]
 end
 
-# Crossover function
+# Crossover function: Can be one-point, multi-point, Davis Order Crossover (OX1), uniform, Whole Arithmetic Recombination
 function crossover(parent1::PackagesStream, parent2::PackagesStream)
     n = length(parent1.packages)
     crossover_point = rand(1:n)
