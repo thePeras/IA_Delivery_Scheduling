65184c16fd6c756861a9214e223cd2631202f830
diogotvf7 <diogotvf7@gmail.com>
2024-03-31 00:38:20 +0000

    Finito mosquito

--- src/app.jl
+++ src/app.jl
@@ -51,100 +51,135 @@ app.layout = html_div() do
     ), 
     html_div(
         children=[
-            html_label(
-                "Map Size: ",
-                style=Dict(
-                    "color"=>"#FFFFFF",
-                    "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                )    
-            ),
-            dcc_input(
-                id="map_size",
-                type="number",
-                min = 20,
-                max = 200,
-                value = 60,
-            ),
-            html_label(
-                "Velocity: ",
-                style=Dict(
-                    "color"=>"#FFFFFF",
-                    "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                )    
-            ),
-            dcc_input(
-                id="velocity",
-                type="number",
-                min = 20,
-                max = 200,
-                value = 60,
-            ),
-            html_label(
-                "Number of Packages: ",
-                style=Dict(
-                    "color"=>"#FFFFFF",
-                    "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                )    
-            ),
-            dcc_input(
-                id="num_packages",
-                type="number",
-                min = 100,
-                max = 5000,
-                value = 1000,
-            ),
-            html_label(
-                "Number of Iterations: ",
-                style=Dict(
-                    "color"=>"#FFFFFF",
-                    "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                )    
-            ),
-            dcc_input(
-                id="num_iterations",
-                type="number",
-                min = 500,
-                max = 10000,
-                value = 1000,
-            ),
-        ],
-        style=Dict(
-            "backgroundColor"=>"#51829B",
-            "padding"=>"20px",
-            "margin"=>"20px",
-            "borderRadius"=>"10px",
-        )
-    ),
-    html_div(
-        id="bind-tuning",
-        children=[
-            html_h2(
-                "Bind Tuning",
-                style=Dict(
-                    "margin"=>"0",
-                    "color"=>"#FFFFFF",
-                    "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                )   
-            ),
             html_div(
                 children=[
-                    tabu_bt(),
-                    simulated_annealing_bt(),
-                    genetic_algorithm_bt(),
+                    html_div(
+                        children=[
+                            html_label(
+                                "Map Size: ",
+                                style=Dict(
+                                    "color"=>"#FFFFFF",
+                                    "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                                )    
+                            ),
+                            dcc_input(
+                                id="map_size",
+                                type="number",
+                                min = 20,
+                                max = 200,
+                                value = 60,
+                            )
+                        ]
+                    ),
+                    html_div(
+                        children=[
+                            html_label(
+                                "Velocity: ",
+                                style=Dict(
+                                    "color"=>"#FFFFFF",
+                                    "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                                )    
+                            ),
+                            dcc_input(
+                                id="velocity",
+                                type="number",
+                                min = 20,
+                                max = 200,
+                                value = 60,
+                            )
+                        ]
+                    ),
+                    html_div(
+                        children=[
+                            html_label(
+                                "Number of Packages: ",
+                                style=Dict(
+                                    "color"=>"#FFFFFF",
+                                    "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                                )    
+                            ),
+                            dcc_input(
+                                id="num_packages",
+                                type="number",
+                                min = 100,
+                                max = 5000,
+                                value = 1000,
+                            )
+                        ]
+                    ),
+                    html_div(
+                        children=[
+                            html_label(
+                                "Number of Iterations: ",
+                                style=Dict(
+                                    "color"=>"#FFFFFF",
+                                    "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                                )    
+                            ),
+                            dcc_input(
+                                id="num_iterations",
+                                type="number",
+                                min = 500,
+                                max = 10000,
+                                value = 1000,
+                            ),
+                        ]
+                    ),
+                    html_button(
+                        "Submit",
+                        id="submit",
+                        n_clicks=0
+                    ),
                 ],
                 style=Dict(
                     "display"=>"flex",
-                    "alignItems"=>"center",
+                    "alignItems"=>"flex-start",
                     "justifyContent"=>"center",
-                    "gap"=>"2em",
+                    "gap"=>"1em",
+                    "flexDirection"=>"column",
+                    "backgroundColor"=>"#51829B",
+                    "padding"=>"20px",
+                    "margin"=>"20px",
+                    "borderRadius"=>"10px",
                 )
-            )
+            ),
+            html_div(
+                id="bind-tuning",
+                children=[
+                    html_h2(
+                        "Bind Tuning",
+                        style=Dict(
+                            "margin"=>"0",
+                            "color"=>"#FFFFFF",
+                            "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                        )   
+                    ),
+                    html_div(
+                        children=[
+                            tabu_bt(),
+                            simulated_annealing_bt(),
+                            genetic_algorithm_bt(),
+                        ],
+                        style=Dict(
+                            "display"=>"flex",
+                            "alignItems"=>"center",
+                            "justifyContent"=>"center",
+                            "gap"=>"1em",
+                        )
+                    )
+                ],
+                style=Dict(
+                    "backgroundColor"=>"#51829B",
+                    "padding"=>"20px",
+                    "margin"=>"20px",
+                    "borderRadius"=>"10px",
+                )
+            ),
         ],
         style=Dict(
-            "backgroundColor"=>"#51829B",
-            "padding"=>"20px",
-            "margin"=>"20px",
-            "borderRadius"=>"10px",
+            "display"=>"flex",
+            "alignItems"=>"center",
+            "justifyContent"=>"center",
         )
     ),
     html_div(
@@ -161,6 +196,7 @@ end
 callback!(
     app,
     Output("graph", "children"),
+    Output("submit", "n_clicks"),
     Input("num_packages", "value"),
     Input("map_size", "value"),
     Input("velocity", "value"),
@@ -171,10 +207,11 @@ callback!(
     Input("cooling_rate", "value"),
     Input("elitism_population_size", "value"),
     Input("mutation_rate", "value"),
-    Input("n_population", "value")
-) do num_packages, map_size, velocity, num_iterations, tabu_size, n_neighbors, initial_temperature, cooling_rate, elitism_population_size, mutation_rate, n_population
-    if (isnothing(num_packages) || isnothing(map_size) || isnothing(velocity) || isnothing(num_iterations) || isnothing(tabu_size) || isnothing(n_neighbors) || isnothing(initial_temperature) || isnothing(elitism_population_size) || isnothing(mutation_rate) || isnothing(cooling_rate) || isnothing(n_population))
-        return ""
+    Input("n_population", "value"),
+    Input("submit", "n_clicks"),
+) do num_packages, map_size, velocity, num_iterations, tabu_size, n_neighbors, initial_temperature, cooling_rate, elitism_population_size, mutation_rate, n_population, n_clicks
+    if (n_clicks == 0 || isnothing(num_packages) || isnothing(map_size) || isnothing(velocity) || isnothing(num_iterations) || isnothing(tabu_size) || isnothing(n_neighbors) || isnothing(initial_temperature) || isnothing(elitism_population_size) || isnothing(mutation_rate) || isnothing(cooling_rate) || isnothing(n_population))
+        return [], 0
     end
 
     packages_stream = generate_package_stream(num_packages, map_size)
@@ -185,16 +222,20 @@ callback!(
     simulated_annealing_data = simulated_annealing(initial_state, Int64(num_iterations), Float64(initial_temperature), Float64(cooling_rate))
     genetic_algorithm_data = genetic_algorithm(initial_state, Int64(n_population), Int64(num_iterations), Int64(elitism_population_size), Float64(mutation_rate))
 
-    return dcc_graph(
-        figure=(
-            data=[
-                (x = ["Tabu Search", "Hill Climbing", "Simulated Annealing", "Genetic Algorithm"], y = [tabu_data.total_distance, hill_climbing_data.total_distance, simulated_annealing_data.total_distance, genetic_algorithm_data.total_distance], type="bar", name="Total Distance"),
-                (x = ["Tabu Search", "Hill Climbing", "Simulated Annealing", "Genetic Algorithm"], y = [length(tabu_data.broken_packages), length(hill_climbing_data.broken_packages), length(simulated_annealing_data.broken_packages), length(genetic_algorithm_data.broken_packages)], type="bar", name="Broken Packages"),
-                (x = ["Tabu Search", "Hill Climbing", "Simulated Annealing", "Genetic Algorithm"], y = [tabu_data.total_time, hill_climbing_data.total_time, simulated_annealing_data.total_time, genetic_algorithm_data.total_time], type="bar", name="Total Time"),
-            ],
-            layout = (title = "Algorithms Comparison", barmode="group")
-        )
-    )
+    return
+        [
+            dcc_graph(
+                figure=(
+                    data=[
+                        (x = ["Tabu Search", "Hill Climbing", "Simulated Annealing", "Genetic Algorithm"], y = [tabu_data.total_distance, hill_climbing_data.total_distance, simulated_annealing_data.total_distance, genetic_algorithm_data.total_distance], type="bar", name="Total Distance"),
+                        (x = ["Tabu Search", "Hill Climbing", "Simulated Annealing", "Genetic Algorithm"], y = [tabu_data.total_time, hill_climbing_data.total_time, simulated_annealing_data.total_time, genetic_algorithm_data.total_time], type="bar", name="Total Time"),
+                        (x = ["Tabu Search", "Hill Climbing", "Simulated Annealing", "Genetic Algorithm"], y = [length(tabu_data.broken_packages), length(hill_climbing_data.broken_packages), length(simulated_annealing_data.broken_packages), length(genetic_algorithm_data.broken_packages)], type="bar", name="Broken Packages"),
+                    ],
+                    layout = (title = "Algorithms Comparison", barmode="group")
+                )
+            )
+        ], 
+        0
 end
 
-run_server(app, "0.0.0.0", 8000, debug=true)
+run_server(app, "0.0.0.0", 8100, debug=true)
--- src/bind_tuning.jl
+++ src/bind_tuning.jl
@@ -13,33 +13,41 @@ module BindTuning
                         "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
                     )   
                 ),
-                html_label(
-                    "Max Tabu Size: ",
-                    style=Dict(
-                        "color"=>"#FFFFFF",
-                        "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                    )    
+                html_div(
+                    children=[
+                        html_label(
+                            "Max Tabu Size: ",
+                            style=Dict(
+                                "color"=>"#FFFFFF",
+                                "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                            )    
+                        ),
+                        dcc_input(
+                            type="number",
+                            id="tabu_size",
+                            min = 10,
+                            max = 100,
+                            value = 10,
+                        ),
+                    ]
                 ),
-                dcc_input(
-                    type="number",
-                    id="tabu_size",
-                    min = 10,
-                    max = 100,
-                    value = 10,
-                ),
-                html_label(
-                    "Number of neighbors: ",
-                    style=Dict(
-                        "color"=>"#FFFFFF",
-                        "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                    )    
-                ),
-                dcc_input(
-                    type="number",
-                    id="n_neighbors",
-                    min = 10,
-                    max = 50,
-                    value = 10,
+                html_div(
+                    children=[
+                        html_label(
+                            "Number of neighbors: ",
+                            style=Dict(
+                                "color"=>"#FFFFFF",
+                                "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                            )    
+                        ),
+                        dcc_input(
+                            type="number",
+                            id="n_neighbors",
+                            min = 10,
+                            max = 50,
+                            value = 10,
+                        )
+                    ]
                 )
             ],
             style=Dict(
@@ -63,33 +71,43 @@ module BindTuning
                         "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
                     )   
                 ),
-                html_label(
-                    "Initial Temperature: ",
-                    style=Dict(
-                        "color"=>"#FFFFFF",
-                        "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                    )    
+                html_div(
+                    children=[
+                        html_label(
+                            "Initial Temperature: ",
+                            style=Dict(
+                                "color"=>"#FFFFFF",
+                                "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                            )    
+                        ),
+                        dcc_input(
+                            type="number",
+                            id="initial_temperature",
+                            min = 0,
+                            max = 100.0,
+                            value = 99.9,
+                            step=0.5,
+                        ),
+                    ]
                 ),
-                dcc_input(
-                    type="number",
-                    id="initial_temperature",
-                    min = 0,
-                    max = 100.0,
-                    value = 99.9,
-                ),
-                html_label(
-                    "Cooling Rate: ",
-                    style=Dict(
-                        "color"=>"#FFFFFF",
-                        "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                    )    
-                ),
-                dcc_input(
-                    type="number",
-                    id="cooling_rate",
-                    min = 0,
-                    max = 1,
-                    value = 0.9,
+                html_div(
+                    children=[
+                        html_label(
+                            "Cooling Rate: ",
+                            style=Dict(
+                                "color"=>"#FFFFFF",
+                                "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                            )    
+                        ),
+                        dcc_input(
+                            type="number",
+                            id="cooling_rate",
+                            min = 0,
+                            max = 1,
+                            value = 0.9,
+                            step=0.1,
+                        )
+                    ]
                 )
             ],
             style=Dict(
@@ -113,47 +131,60 @@ module BindTuning
                         "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
                     )   
                 ),
-                html_label(
-                    "Number of Population: ",
-                    style=Dict(
-                        "color"=>"#FFFFFF",
-                        "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                    )    
+                html_div(
+                    children=[
+                        html_label(
+                            "Number of Population: ",
+                            style=Dict(
+                                "color"=>"#FFFFFF",
+                                "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                            )    
+                        ),
+                        dcc_input(
+                            type="number",
+                            id="n_population",
+                            min = 10,
+                            max = 150,
+                            value = 50,
+                        ),
+                    ]
                 ),
-                dcc_input(
-                    type="number",
-                    id="n_population",
-                    min = 10,
-                    max = 150,
-                    value = 50,
+                html_div(
+                    children=[
+                        html_label(
+                            "Elitism Population Size: ",
+                            style=Dict(
+                                "color"=>"#FFFFFF",
+                                "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                            )    
+                        ),
+                        dcc_input(
+                            type="number",
+                            id="elitism_population_size",
+                            min = 0,
+                            max = 50,
+                            value = 20,
+                        ),
+                    ]
                 ),
-                html_label(
-                    "Elitism Population Size: ",
-                    style=Dict(
-                        "color"=>"#FFFFFF",
-                        "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                    )    
-                ),
-                dcc_input(
-                    type="number",
-                    id="elitism_population_size",
-                    min = 0,
-                    max = 50,
-                    value = 20,
-                ),
-                html_label(
-                    "Mutation Rate: ",
-                    style=Dict(
-                        "color"=>"#FFFFFF",
-                        "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
-                    )    
-                ),
-                dcc_input(
-                    type="number",
-                    id="mutation_rate",
-                    min = 0,
-                    max = 1.0,
-                    value = 0.01,
+                html_div(
+                    children=[
+                        html_label(
+                            "Mutation Rate: ",
+                            style=Dict(
+                                "color"=>"#FFFFFF",
+                                "fontFamily"=>"Source Sans Pro, Arial, sans-serif",
+                            )    
+                        ),
+                        dcc_input(
+                            type="number",
+                            id="mutation_rate",
+                            min = 0,
+                            max = 1.0,
+                            value = 0.01,
+                            step=0.01,
+                        )
+                    ]
                 )
             ],
             style=Dict(
